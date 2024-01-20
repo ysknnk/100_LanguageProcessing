c7237adeddc029879747ba4ca2f487afa3961dfb
ysknnk <nnk.g.41@gmail.com>
2024-01-20 13:21:39 +0900

    7本目

---
+++ 08_Cryptography/ans.py
@@ -0,0 +1,22 @@
+# 2024年1月20日
+"""
+与えられた文字列の各文字を，以下の仕様で変換する関数cipherを実装せよ．
+
+英小文字ならば(219 - 文字コード)の文字に置換
+その他の文字はそのまま出力
+この関数を用い，英語のメッセージを暗号化・復号化せよ．
+"""
+
+
+def cipher(s):
+    val = ''
+    for i in s:
+        if i.islower():
+            # 219 - 文字コード
+            i = chr(219 - ord(i))
+        val += i
+    return val
+
+
+print(cipher('Apple'))
+print(cipher(cipher('Apple')))
