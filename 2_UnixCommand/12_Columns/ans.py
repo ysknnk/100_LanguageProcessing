23e73446a052b95a648ddd069bc6f1788c6859b6
ysknnk <nnk.g.41@gmail.com>
2024-01-23 18:05:22 +0900

    12本目

---
+++ 2_UnixCommand/12_Columns/ans.py
@@ -0,0 +1,13 @@
+# 2024年1月23日
+"""
+各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
+"""
+
+f = open('2_UnixCommand/12_Columns/popular-names.txt', 'r')
+col1 = open('2_UnixCommand/12_Columns/col1.txt', 'w')
+col2 = open('2_UnixCommand/12_Columns/col2.txt', 'w')
+
+for i in f:
+    ii = i.split('\t')
+    col1.write(ii[0] + '\n')
+    col2.write(ii[1] + '\n')
