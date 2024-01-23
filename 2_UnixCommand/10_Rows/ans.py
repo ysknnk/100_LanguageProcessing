aa9bf384a3c2a75adb4afa277d081fc4de75f8e7
ysknnk <nnk.g.41@gmail.com>
2024-01-23 17:17:00 +0900

    コメント修正

--- 2_UnixCommand/10_Rows/ans.py
+++ 2_UnixCommand/10_Rows/ans.py
@@ -7,12 +7,5 @@ f = open('2_UnixCommand/10_Rows/popular-names.txt', 'r')
 
 print(len(list(f)))
 
-
-# powershellなので・・・
-# PS C:\Users\nnkg4\pythonProjects\100_LanguageProcessing> (cat .\2_UnixCommand\popular-names.txt).Length
-# 2780
-
-# ↑の後にちゃんとlinuxコマンド使えるようにしました・・・
-# 詳しくは  100_LanguageProcessing/2_UnixCommand/10_Rows/README.md
 # wc -l 2_UnixCommand/popular-names.txt
 # 2780 2_UnixCommand/popular-names.txt
