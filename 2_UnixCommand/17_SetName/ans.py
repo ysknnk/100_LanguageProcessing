9883fd0718563b5a8ec718c8b9b42c65a008e50f
ysknnk <nnk.g.41@gmail.com>
2024-01-29 16:39:42 +0900

    17本目の中身を修正

--- 2_UnixCommand/17_SetName/ans.py
+++ 2_UnixCommand/17_SetName/ans.py
@@ -3,7 +3,7 @@
 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはcut, sort, uniqコマンドを用いよ．
 """
 
-f = open('2_UnixCommand/17_DiffLetters/popular-names.txt', 'r')
+f = open('2_UnixCommand/17_SetName/popular-names.txt', 'r')
 tmp_l = []
 
 for i in f:
@@ -16,6 +16,6 @@ print(sorted(set(tmp_l)))
 ['Abigail', 'Aiden', 'Alexander', 'Alexis', 'Alice', 'Amanda', 'Amelia', 'Amy', 'Andrew', 'Angela', 'Anna', 'Annie', 'Anthony', 'Ashley', 'Austin', 'Ava', 'Barbara', 'Benjamin', 'Bertha', 'Bessie', 'Betty', 'Brandon', 'Brian', 'Brittany', 'Carol', 'Carolyn', 'Charles', 'Charlotte', 'Chloe', 'Christopher', 'Clara', 'Crystal', 'Cynthia', 'Daniel', 'David', 'Deborah', 'Debra', 'Donald', 'Donna', 'Doris', 'Dorothy', 'Edward', 'Elijah', 'Elizabeth', 'Emily', 'Emma', 'Ethan', 'Ethel', 'Evelyn', 'Florence', 'Frances', 'Frank', 'Gary', 'George', 'Hannah', 'Harper', 'Harry', 'Heather', 'Helen', 'Henry', 'Ida', 'Isabella', 'Jacob', 'James', 'Jason', 'Jayden', 'Jeffrey', 'Jennifer', 'Jessica', 'Joan', 'John', 'Joseph', 'Joshua', 'Judith', 'Julie', 'Justin', 'Karen', 'Kathleen', 'Kelly', 'Kimberly', 'Larry', 'Laura', 'Lauren', 'Liam', 'Lillian', 'Linda', 'Lisa', 'Logan', 'Lori', 'Lucas', 'Madison', 'Margaret', 'Marie', 'Mark', 'Mary', 'Mason', 'Matthew', 'Megan', 'Melissa', 'Mia', 'Michael', 'Michelle', 'Mildred', 'Minnie', 'Nancy', 'Nicholas', 'Nicole', 'Noah', 'Oliver', 'Olivia', 'Pamela', 'Patricia', 'Rachel', 'Rebecca', 'Richard', 'Robert', 'Ronald', 'Ruth', 'Samantha', 'Sandra', 'Sarah', 'Scott', 'Sharon', 'Shirley', 'Sophia', 'Stephanie', 'Steven', 'Susan', 'Tammy', 'Taylor', 'Thomas', 'Tracy', 'Tyler', 'Virginia', 'Walter', 'William']
 
 # cut, sort, uniq
-cut -f 1 2_UnixCommand/17_DiffLetters/popular-names.txt | sort | uniq | paste -d, -s
+cut -f 1 2_UnixCommand/17_SetName/popular-names.txt | sort | uniq | paste -d, -s
 Abigail,Aiden,Alexander,Alexis,Alice,Amanda,Amelia,Amy,Andrew,Angela,Anna,Annie,Anthony,Ashley,Austin,Ava,Barbara,Benjamin,Bertha,Bessie,Betty,Brandon,Brian,Brittany,Carol,Carolyn,Charles,Charlotte,Chloe,Christopher,Clara,Crystal,Cynthia,Daniel,David,Deborah,Debra,Donald,Donna,Doris,Dorothy,Edward,Elijah,Elizabeth,Emily,Emma,Ethan,Ethel,Evelyn,Florence,Frances,Frank,Gary,George,Hannah,Harper,Harry,Heather,Helen,Henry,Ida,Isabella,Jacob,James,Jason,Jayden,Jeffrey,Jennifer,Jessica,Joan,John,Joseph,Joshua,Judith,Julie,Justin,Karen,Kathleen,Kelly,Kimberly,Larry,Laura,Lauren,Liam,Lillian,Linda,Lisa,Logan,Lori,Lucas,Madison,Margaret,Marie,Mark,Mary,Mason,Matthew,Megan,Melissa,Mia,Michael,Michelle,Mildred,Minnie,Nancy,Nicholas,Nicole,Noah,Oliver,Olivia,Pamela,Patricia,Rachel,Rebecca,Richard,Robert,Ronald,Ruth,Samantha,Sandra,Sarah,Scott,Sharon,Shirley,Sophia,Stephanie,Steven,Susan,Tammy,Taylor,Thomas,Tracy,Tyler,Virginia,Walter,William
 """
