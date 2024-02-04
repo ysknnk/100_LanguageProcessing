7f8581b70a980aea71df13c8e4fbb68989707163
ysknnk <nnk.g.41@gmail.com>
2024-02-04 13:13:37 +0900

    25本目

---
+++ 3_RegularExpression/25_Template/ans.py
@@ -0,0 +1,24 @@
+# 2024年2月4日
+"""
+記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ．
+"""
+
+import pandas as pd
+import re
+
+df = pd.read_json(
+    '3_RegularExpression/25_Template/jawiki-country.json.gz', lines=True)
+
+uk = df.query('title=="イギリス"')['text'].values[0]
+
+ans = {}
+
+for index, i in enumerate(uk.split('\n')):
+    pattern = '^\|(.*)\s=\s*(.*)'
+    template = re.search(pattern, i)
+    if '略名' in i:
+        print(i)
+    if template:
+        ans[template.group(1)] = template.group(2)
+
+print(ans)
